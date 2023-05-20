08df8c8e269146778b1f41c7aacfb5e318fbea74
remplerus <44590574+remplerus@users.noreply.github.com>
2023-05-21 01:24:50 +0200

    update to get textures work
    next step: make speedier conveyors work

--- src/main/java/com/rempler/jaiea/common/block/SuperSpeedyConveyor.java
+++ src/main/java/com/rempler/jaiea/common/block/SuperSpeedyConveyor.java
@@ -4,13 +4,12 @@ import blusunrize.immersiveengineering.api.tool.conveyor.BasicConveyorType;
 import blusunrize.immersiveengineering.api.tool.conveyor.IConveyorType;
 import blusunrize.immersiveengineering.client.render.conveyor.BasicConveyorRender;
 import blusunrize.immersiveengineering.common.blocks.metal.conveyors.ConveyorBase;
+import com.rempler.jaiea.utils.JAIEAConstants;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
-import static blusunrize.immersiveengineering.ImmersiveEngineering.MODID;
-
 public class SuperSpeedyConveyor extends ConveyorBase {
-    public static final ResourceLocation NAME = new ResourceLocation(MODID, "dropper");
+    public static final ResourceLocation NAME = new ResourceLocation(JAIEAConstants.MODID, "super_speedy");
     public static ResourceLocation texture_on = new ResourceLocation("jaiea:block/conveyor/super_speedy");
     public static ResourceLocation texture_off = new ResourceLocation("jaiea:block/conveyor/super_speedy_off");
     public static final IConveyorType<SuperSpeedyConveyor> TYPE = new BasicConveyorType<>(
